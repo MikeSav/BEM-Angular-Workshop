ab10a6df60da837f672aaf9c83491880750b9062
Mike Savage <michael.savage@msasafety.com>
2018-08-13 16:22:26 +0200

    Added more form items...

--- src/app/shared/directives/msa-input.directive.ts
+++ src/app/shared/directives/msa-input.directive.ts
@@ -31,7 +31,7 @@ export class MsaInputDirective {
                 Element.prototype.msMatchesSelector ||
                 Element.prototype.webkitMatchesSelector ||
                 function (s) {
-                    const matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                    let matches = (this.document || this.ownerDocument).querySelectorAll(s),
                         i = matches.length;
                     while (--i >= 0 && matches.item(i) !== this) {
                     }
