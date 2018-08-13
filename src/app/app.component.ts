548656d5d30c02f611e00d4459a21d59bee273ca
Mike Savage <michael.savage@msasafety.com>
2018-08-13 16:44:47 +0200

    Removed issue with menu toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'msa-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-    public closeContent = true;
+  public closeContent = true;
 
-    public toggleMenu() {
-      this.closeContent = !this.closeContent;
-    }
+  public toggleMenu() {
+    this.closeContent = !this.closeContent;
+  }
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.closeContent = true;
+      }
+    });
+  }
 }
